f42f9936fe2a01597cfd83b9c7a50eb6ba75fa6d
Remi Pilon <remi.pilon22@gmail.com>
2024-04-10 15:26:53 -0400

    php

---
+++ connect.php
@@ -0,0 +1,15 @@
+<?php
+    $DATABSE_HOST = "localhost";
+    $DATABSE_USER = "root";
+    $DATABSE_PSW = "";
+    $DATABASE_NAME = "resn_db";
+
+    $connection = new MYSQLi($DATABSE_HOST, $DATABSE_USER, $DATABSE_PSW, $DATABASE_NAME);
+    
+    if(!$connection) {
+        die("Connection failed: " . mysqli_connect_error());
+        // In the future we will redirect to a page that will tell the user that there was an error.
+    } else {
+        echo "Connected successfully";
+    }
+?>
